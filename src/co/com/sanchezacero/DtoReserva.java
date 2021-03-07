93413f95cafbffc81771a885c62bad1f0325915f
Carlos <carlos.sanchez.a0@gmail.com>
2021-03-07 15:21:10 -0500

    crear editar me falta listar

--- src/co/com/sanchezacero/DtoReserva.java
+++ src/co/com/sanchezacero/DtoReserva.java
@@ -1,5 +1,90 @@
 package co.com.sanchezacero;
 
+import java.util.Date;
+
 public class DtoReserva {
+    private int noDocumento;
+    private String nombreCliente;
+    private String noContacto;
+    private String correo;
+    private Date fechaReserva;
+    private String estado;
+    private int cantidadPersonas;
+    private String motivo;
+    private String tipoDecoracion;
+
+    public DtoReserva(){}
+
+    public String getTipoDecoracion() {
+        return tipoDecoracion;
+    }
+
+    public void setTipoDecoracion(String tipoDecoracion) {
+        this.tipoDecoracion = tipoDecoracion;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public int getCantidadPersonas() {
+        return cantidadPersonas;
+    }
+
+    public void setCantidadPersonas(int cantidadPersonas) {
+        this.cantidadPersonas = cantidadPersonas;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Date getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(Date fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNoContacto() {
+        return noContacto;
+    }
+
+    public void setNoContacto(String noContacto) {
+        this.noContacto = noContacto;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public int getNoDocumento() {
+        return noDocumento;
+    }
+
+    public void setNoDocumento(int noDocumento) {
+        this.noDocumento = noDocumento;
+    }    
     
 }
--- src/co/com/sanchezacero/Reservas.java
+++ src/co/com/sanchezacero/Reservas.java
@@ -1,5 +1,11 @@
 package co.com.sanchezacero;
 import java.sql.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Reservas {
     
     Connection conn;
@@ -9,11 +15,12 @@ public class Reservas {
     String mariaUser = "root";
     String mariaPassword = "password";
 
+    static Scanner s;
     public Connection conectar(){
         try {
             Class.forName("org.mariadb.jdbc.Driver");
             conn = DriverManager.getConnection(mariaUrl,mariaUser,mariaPassword);
-            System.out.println("Conectado a la base de datos");
+            // System.out.println("Conectado a la base de datos");
         } catch (ClassNotFoundException|SQLException ex) {
             System.out.println(ex);
             System.out.println("No conectao a la base de datos");
@@ -28,22 +35,206 @@ public class Reservas {
             System.out.println("No pudo desconectar de la base de datos");
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        s  = new Scanner(System.in);
+        DtoReserva dtoReserva = new DtoReserva();
+        int opcion = menuOpciones();
+        System.out.println("Has seleccionado: " + opcion);
 
-        try {  
-            Reservas mariaCx = new Reservas();
-            mariaCx.conectar();
+        while (opcion != 5) {
+            if(opcion == 1){
+                System.out.println("Seleccionaste crear reserva");
+                dtoReserva = new DtoReserva();
+                
+                System.out.println("Digite su identificacion");
+                dtoReserva.setNoDocumento(s.nextInt());
+                s.nextLine();
+                System.out.println("Digite nombre del cliente");
+                String nnombre = s.nextLine();
+                dtoReserva.setNombreCliente(nnombre);
+                
+                System.out.println("Digite numero de telefono del cliente");
+                dtoReserva.setNoContacto(s.nextLine());
+                
+                System.out.println("Digite correo electronico");
+                dtoReserva.setCorreo(s.nextLine());
+                
+                System.out.println("Digite fecha en formato dd/mm/yyyy");
+                SimpleDateFormat fechaScanner = new SimpleDateFormat("dd/MM/yyyy");
+                Date fechaFormat = fechaScanner.parse(s.nextLine());
+                dtoReserva.setFechaReserva(fechaFormat);
+                
+
+                System.out.println("Seleccione estado");
+                System.out.println("1. activo");
+                System.out.println("2. inactivo");
+                int seleccion = s.nextInt();
+                
+                if(seleccion == 1){
+                    dtoReserva.setEstado("activo");
+                } else {
+                    dtoReserva.setEstado("inactivo");
+                }
+
+                System.out.println("Digite cantidad de personas");
+                dtoReserva.setCantidadPersonas(s.nextInt());
+                
+                s.nextLine();
+                System.out.println("Digite motivo");
+                dtoReserva.setMotivo(s.nextLine());
+
+                System.out.println("Digite tipo de decoracion");
+                dtoReserva.setTipoDecoracion(s.nextLine());
+
+
+                System.out.println("");
+                System.out.println("Resumen de la reserva");
+                System.out.println("");
+                System.out.println("Documento: "+dtoReserva.getNoDocumento());
+                System.out.println("Nombre: "+dtoReserva.getNombreCliente());
+                System.out.println("Telefono: "+dtoReserva.getNoContacto());
+                System.out.println("Correo electronico: "+dtoReserva.getCorreo());
+                System.out.println("Fecha: "+dtoReserva.getFechaReserva());
+                System.out.println("Estado: "+dtoReserva.getEstado());
+                System.out.println("Cantidad de personas: "+dtoReserva.getCantidadPersonas());
+                System.out.println("Motivo: "+dtoReserva.getMotivo());
+                System.out.println("Tipo decoracion: "+dtoReserva.getTipoDecoracion());
+                System.out.println("");
+                
+                crearReserva(dtoReserva);
             
-            Statement stmt = mariaCx.conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM reservas");
-            while (rs.next()) {
-                System.out.println(rs.getInt("id"));
+            }  else if (opcion == 2){
+                System.out.println("Seleccionaste cancelar reserva");
+                System.out.println("Ingrese el numero de reserva para cancelar");
+                
+                cancelarReserva(s.nextInt());
+            } else if (opcion == 3) {
+                System.out.println("Seleccionaste modificar reserva");
+                System.out.println("Ingresa el numero de la reserva");
+                int id = s.nextInt();
+                
+                s.nextLine();
+                System.out.println("Digite fecha en formato dd/mm/yyyy");
+                SimpleDateFormat fechaScanner = new SimpleDateFormat("dd/MM/yyyy");
+                Date fechaFormat = fechaScanner.parse(s.nextLine());
+                dtoReserva.setFechaReserva(fechaFormat);
+                
+                System.out.println("Digite cantidad de personas");
+                dtoReserva.setCantidadPersonas(s.nextInt());
+
+                s.nextLine();
+                System.out.println("Digite motivo");
+                dtoReserva.setMotivo(s.nextLine());
+
+                System.out.println("Digite tipo de decoracion");
+                dtoReserva.setTipoDecoracion(s.nextLine());
+
+                modificarReserva(dtoReserva,id);
             }
-            System.out.println("Hello, World!");
-            mariaCx.desconectar();
-        } catch (Exception e) {
-            System.out.println(e);
+            opcion = menuOpciones();
+        }
+
+        // try {  
+        //     Reservas mariaCx = new Reservas();
+        //     mariaCx.conectar();
+            
+        //     Statement stmt = mariaCx.conn.createStatement();
+        //     ResultSet rs = stmt.executeQuery("SELECT * FROM reservas");
+        //     while (rs.next()) {
+        //         System.out.println(rs.getInt("id"));
+        //     }
+        //     System.out.println("Hello, World!");
+        //     mariaCx.desconectar();
+            
+        // } catch (Exception e) {
+        //     System.out.println(e);
+        // }
+    }
+    public static int menuOpciones(){
+        int opcion = 0;
+        while (opcion < 1 || opcion > 5) {
+            System.out.println("");
+            System.out.println("Elige una opcion:");
+            System.out.println("");
+            System.out.println("1. Crear reserva");
+            System.out.println("2. Cancelar reserva");
+            System.out.println("3. Modificar reserva");
+            System.out.println("4. Consultar reservas");
+            System.out.println("5. Salir");
+
+            opcion = s.nextInt();
+        }
+        return opcion;
+    }
+    public static void crearReserva(DtoReserva dtoReserva) {
+        System.out.println("procesando...");
+
+        Reservas mariaCx = new Reservas();
+        mariaCx.conectar();
+        String queryInsert = "INSERT INTO reservas (identificacion,nombreCliente,noContacto,correo,fechaReserva,estado,cantidadPersonas,motivo,tipoDecoracion) VALUES (?,?,?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = mariaCx.conn.prepareStatement(queryInsert, Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1,dtoReserva.getNoDocumento());
+            stmt.setString(2,dtoReserva.getNombreCliente());
+            stmt.setString(3,dtoReserva.getNoContacto());
+            stmt.setString(4,dtoReserva.getCorreo());
+            stmt.setDate(5,new java.sql.Date( dtoReserva.getFechaReserva().getTime()) );
+            stmt.setString(6, dtoReserva.getEstado());
+            stmt.setInt(7, dtoReserva.getCantidadPersonas());
+            stmt.setString(8, dtoReserva.getMotivo());
+            stmt.setString(9, dtoReserva.getTipoDecoracion());
+            stmt.execute();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            while (rs.next()){
+                System.out.println("El numero de reserva es: "+rs.getString(1));
+            }
+            mariaCx.conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("NO INSERTO RESERVA");
+        }
+        
+        System.out.println("***Reserva creada con éxito***");
+    }
+    public static void cancelarReserva(int id) {
+        System.out.println("procesando...");
+        Reservas mariaCx = new Reservas();
+        mariaCx.conectar();
+        String queryUpdate = "UPDATE reservas SET estado = ? WHERE id = ? LIMIT 1";
+        try {
+            PreparedStatement stmt = mariaCx.conn.prepareStatement(queryUpdate);
+            stmt.setString(1, "inactivo");
+            stmt.setInt(2, id);
+            stmt.execute();
+            mariaCx.conn.close();
+            System.out.println("Reserva "+id+" cancelada con exito");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("No es posible cancelar la reserva");
         }
     }
+    public static void modificarReserva(DtoReserva dtoReserva, int id) {
+        Reservas mariaCx = new Reservas();
+        mariaCx.conectar();
+        String query = "UPDATE reservas SET fechaReserva = ?, cantidadPersonas=?, motivo=?, tipoDecoracion=? WHERE id=?";
+        try {
+            PreparedStatement stmt = mariaCx.conn.prepareStatement(query);
+            stmt.setDate(1, new java.sql.Date(dtoReserva.getFechaReserva().getTime()));
+            stmt.setInt(2, dtoReserva.getCantidadPersonas());
+            stmt.setString(3, dtoReserva.getMotivo());
+            stmt.setString(4, dtoReserva.getTipoDecoracion());
+            stmt.setInt(5, id);
+            stmt.execute();
+
+            System.out.println("Actualizado con exito");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void listarReserva() {
+        
+    }
+    
     
 }
